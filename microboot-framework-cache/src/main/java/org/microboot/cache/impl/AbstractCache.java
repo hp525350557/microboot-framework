a42c7f92a9daebfef8fa465e50cdeeb79a4a72a8
hp525350557 <525350557@qq.com>
2022-11-16 10:44:46 +0800

    1、AbstractCache改为继承AbstractValueAdaptingCache 2、去掉了AbstractCache中一些多余方法

--- microboot-framework-cache/src/main/java/org/microboot/cache/impl/AbstractCache.java
+++ microboot-framework-cache/src/main/java/org/microboot/cache/impl/AbstractCache.java
@@ -1,52 +1,43 @@
 package org.microboot.cache.impl;
 
-import org.springframework.cache.Cache;
-import org.springframework.cache.support.SimpleValueWrapper;
+import org.springframework.cache.support.AbstractValueAdaptingCache;
 
 import java.util.concurrent.Callable;
 
 /**
  * @author 胡鹏
  */
-public abstract class AbstractCache implements Cache {
+public abstract class AbstractCache extends AbstractValueAdaptingCache {
 
     //缓存最终生成的key的前缀
     protected String name;
 
-    @Override
-    public ValueWrapper get(Object key) {
-        ValueWrapper wrapper = null;
-        Object value = this.getValue(key);
-        if (value != null) {
-            wrapper = new SimpleValueWrapper(value);
-        }
-        return wrapper;
+    protected AbstractCache() {
+        this(false);
     }
 
-    /**
-     * 无需实现该方法，CacheImpl作为多级缓存的入口并没有调用该方法
-     *
-     * @param key
-     * @param type
-     * @param <T>
-     * @return
-     */
-    @Override
-    public <T> T get(Object key, Class<T> type) {
-        return null;
+    protected AbstractCache(boolean allowNullValues) {
+        super(allowNullValues);
     }
 
-    /**
-     * 无需实现该方法，CacheImpl作为多级缓存的入口并没有调用该方法
-     *
-     * @param key
-     * @param callable
-     * @param <T>
-     * @return
-     */
+    @Override
+    protected Object lookup(Object key) {
+        return this.getValue(key);
+    }
+
     @Override
     public <T> T get(Object key, Callable<T> callable) {
-        return null;
+        ValueWrapper valueWrapper = this.get(key);
+        if (valueWrapper != null) {
+            return (T) valueWrapper.get();
+        }
+        T value;
+        try {
+            value = callable.call();
+        } catch (Exception e) {
+            throw new ValueRetrievalException(key, callable, e);
+        }
+        return value;
     }
 
     @Override
@@ -55,18 +46,6 @@ public abstract class AbstractCache implements Cache {
             return;
         }
         this.setValue(key, value);
-    }
-
-    /**
-     * 无需实现该方法，CacheImpl作为多级缓存的入口并没有调用该方法
-     *
-     * @param key
-     * @param value
-     * @return
-     */
-    @Override
-    public ValueWrapper putIfAbsent(Object key, Object value) {
-        return null;
     }
 
     @Override
