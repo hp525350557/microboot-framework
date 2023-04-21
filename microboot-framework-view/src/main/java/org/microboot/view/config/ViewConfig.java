41cd9506e9b393913e88a0e67b2ae03276d3afc7
hp525350557 <525350557@qq.com>
2023-04-21 11:45:36 +0800

    1、优化View模块的FreeMarker构建过程

--- microboot-framework-view/src/main/java/org/microboot/view/config/ViewConfig.java
+++ microboot-framework-view/src/main/java/org/microboot/view/config/ViewConfig.java
@@ -4,12 +4,11 @@ import com.google.common.collect.Maps;
 import freemarker.ext.beans.BeansWrapperBuilder;
 import org.microboot.core.constant.Constant;
 import org.microboot.view.bean.FreeMarkerConfigurerExtender;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Properties;
 
@@ -20,58 +19,23 @@ import java.util.Properties;
 @DependsOn(Constant.APPLICATION_CONTEXT_HOLDER)
 public class ViewConfig {
 
-    @Value("${custom.resources.views.location:}")
-    private String CUSTOM_RESOURCES_VIEWS_LOCATION;
-
-    /**
-     * 初始化FreeMarkerConfigurerExtender
-     *
-     * @return
-     */
     @Bean(name = "org.microboot.view.bean.FreeMarkerConfigurerExtender")
-    public FreeMarkerConfigurerExtender initFreeMarkerConfigurerExtender() {
+    public FreeMarkerConfigurerExtender initFreeMarkerConfigurerExtender(FreeMarkerProperties properties) {
         FreeMarkerConfigurerExtender configurer = new FreeMarkerConfigurerExtender();
-        configurer.setTemplateLoaderPaths(CUSTOM_RESOURCES_VIEWS_LOCATION);
-        configurer.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        //本来想交给SpringBoot去管理，直接在配置文件中写配置信息即可
+        //但是测试发现beansWrapperFn会报错，可能是因为不是在构建Bean的时候设置的
+        //所以参考SpringBoot的构建方式，手动创建，现在settings属性还是在配置文件中设置
+        configurer.setTemplateLoaderPaths(properties.getTemplateLoaderPath());
+        configurer.setPreferFileSystemAccess(properties.isPreferFileSystemAccess());
+        configurer.setDefaultEncoding(properties.getCharsetName());
+        Properties settings = new Properties();
+        settings.put("recognize_standard_file_extensions", "true");
+        settings.putAll(properties.getSettings());
+        configurer.setFreemarkerSettings(settings);
+        //用于整合Java工具类
         Map<String, Object> variables = Maps.newHashMap();
         variables.put("beansWrapperFn", new BeansWrapperBuilder(freemarker.template.Configuration.VERSION_2_3_24).build());
         configurer.setFreemarkerVariables(variables);
-        Properties freemarkerSettings = new Properties();
-        freemarkerSettings.setProperty("template_update_delay", Constant.CODE_5);
-        freemarkerSettings.setProperty("default_encoding", StandardCharsets.UTF_8.name());
-        freemarkerSettings.setProperty("output_encoding", StandardCharsets.UTF_8.name());
-        freemarkerSettings.setProperty("url_escaping_charset", StandardCharsets.UTF_8.name());
-        freemarkerSettings.setProperty("datetime_format", "yyyy-MM-dd HH:mm:ss");
-        freemarkerSettings.setProperty("date_format", "yyyy-MM-dd");
-        freemarkerSettings.setProperty("time_format", "HH:mm:ss");
-        freemarkerSettings.setProperty("number_format", "#.##");
-        freemarkerSettings.setProperty("tag_syntax", "auto_detect");
-        freemarkerSettings.setProperty("boolean_format", "true,false");
-        freemarkerSettings.setProperty("whitespace_stripping", "true");
-        configurer.setFreemarkerSettings(freemarkerSettings);
         return configurer;
     }
-
-    /**
-     * 初始化FreeMarkerViewResolver
-     * <p>
-     * 如果使用自动装配的方式加载配置类，则不能使用JavaBean的方式自定义FreeMarkerViewResolver
-     * <p>
-     * 另外，如果使用扫描的方式，最好指定FreeMarkerViewResolver的beanName = freeMarkerViewResolver
-     * 因为FreeMarker默认的FreeMarkerViewResolver创建Bean时的条件是@ConditionalOnMissingBean(name = "freeMarkerViewResolver")
-     *
-     * @return
-     */
-    /*
-    @Bean(name = "org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver")
-    public FreeMarkerViewResolver initFreeMarkerViewResolver() {
-        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
-        //resolver.setPrefix("");//设置地址前缀
-        resolver.setSuffix(".html");//设置后缀
-        resolver.setContentType("text/html;charset=UTF-8");
-        resolver.setRequestContextAttribute("request");
-        resolver.setCache(false);
-        return resolver;
-    }
-    */
 }
