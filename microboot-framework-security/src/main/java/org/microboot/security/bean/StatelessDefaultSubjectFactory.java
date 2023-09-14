b77124dc74aec75c24e791a280565a14be718ed4
hp525350557 <525350557@qq.com>
2023-09-15 03:13:57 +0800

    1、修改microboot-framework-security模块禁用Shiro默认Session的逻辑

---
+++ microboot-framework-security/src/main/java/org/microboot/security/bean/StatelessDefaultSubjectFactory.java
@@ -0,0 +1,27 @@
+package org.microboot.security.bean;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.SubjectContext;
+import org.apache.shiro.web.mgt.DefaultWebSubjectFactory;
+
+/**
+ * @author 胡鹏
+ *
+ * 无状态认证需要禁用Session
+ * 参考：https://blog.51cto.com/u_11142439/3085621
+ */
+public class StatelessDefaultSubjectFactory extends DefaultWebSubjectFactory {
+
+    private boolean sessionCreationEnable;
+
+    public StatelessDefaultSubjectFactory(boolean sessionCreationEnabled) {
+        this.sessionCreationEnable = sessionCreationEnabled;
+    }
+
+    @Override
+    public Subject createSubject(SubjectContext context) {
+        //禁止创建session
+        context.setSessionCreationEnabled(this.sessionCreationEnable);
+        return super.createSubject(context);
+    }
+}
--- microboot-framework-security/src/main/java/org/microboot/security/config/SecurityConfig.java
+++ microboot-framework-security/src/main/java/org/microboot/security/config/SecurityConfig.java
@@ -2,14 +2,17 @@ package org.microboot.security.config;
 
 import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.DefaultSessionManager;
 import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.microboot.core.constant.Constant;
+import org.microboot.security.bean.StatelessDefaultSubjectFactory;
 import org.microboot.security.oauth2.OAuth2Filter;
 import org.microboot.security.oauth2.OAuth2Realm;
 import org.springframework.context.annotation.Bean;
@@ -28,12 +31,9 @@ import java.util.Map;
 @DependsOn(Constant.APPLICATION_CONTEXT_HOLDER)
 public class SecurityConfig {
 
-    private final boolean DELETE_INVALID_SESSIONS = false;
     private final boolean SESSION_VALIDATION_SCHEDULER_ENABLED = false;
-    private final boolean SESSION_ID_URL_REWRITING_ENABLED = false;
-    private final boolean SESSION_ID_COOKIE_ENABLED = false;
-    private final long SESSION_VALIDATION_INTERVAL = 3_600_000;
-    private final long GLOBAL_SESSION_TIMEOUT = 1_800_000;
+    private final boolean SESSION_CREATION_ENABLED = false;
+    private final boolean SESSION_STORAGE_ENABLED = false;
 
     private final String OAUTH_URL = "/**";
 
@@ -54,27 +54,11 @@ public class SecurityConfig {
     }
 
     @Bean(name = "org.apache.shiro.session.mgt.SessionManager")
-    public SessionManager initSessionManager(Environment environment) {
-        //本框架由于使用无状态登录，所以要禁用session，下面的属性全都默认设置为false了
-        DefaultWebSessionManager defaultWebSessionManager = new DefaultWebSessionManager();
-        //是否开启删除无效的session对象，缺省值为true
-        defaultWebSessionManager.setDeleteInvalidSessions(environment.getProperty("shiro.sessionManager.deleteInvalidSessions") == null
-                ? DELETE_INVALID_SESSIONS : Boolean.parseBoolean(environment.getProperty("shiro.sessionManager.deleteInvalidSessions")));
+    public SessionManager initSessionManager() {
+        //本框架由于使用无状态登录，所以要禁用session
+        DefaultSessionManager defaultWebSessionManager = new DefaultSessionManager();
         //是否开启定时调度器进行检测过期session，缺省值为true
-        defaultWebSessionManager.setSessionValidationSchedulerEnabled(environment.getProperty("shiro.sessionManager.sessionValidationSchedulerEnabled") == null
-                ? SESSION_VALIDATION_SCHEDULER_ENABLED : Boolean.parseBoolean(environment.getProperty("shiro.sessionManager.sessionValidationSchedulerEnabled")));
-        //是否取消url后面的JSESSIONID，缺省值为true
-        defaultWebSessionManager.setSessionIdUrlRewritingEnabled(environment.getProperty("shiro.sessionManager.sessionIdUrlRewritingEnabled") == null
-                ? SESSION_ID_URL_REWRITING_ENABLED : Boolean.parseBoolean(environment.getProperty("shiro.sessionManager.sessionIdUrlRewritingEnabled")));
-        //是否将shiro的sessionId放在cookie中，缺省值为true
-        defaultWebSessionManager.setSessionIdCookieEnabled(environment.getProperty("shiro.sessionManager.sessionIdCookieEnabled") == null
-                ? SESSION_ID_COOKIE_ENABLED : Boolean.parseBoolean(environment.getProperty("shiro.sessionManager.sessionIdCookieEnabled")));
-        //定时调度器进行检测过期session的扫描时间，缺省值1个小时
-        defaultWebSessionManager.setSessionValidationInterval(environment.getProperty("shiro.sessionManager.sessionValidationInterval") == null
-                ? SESSION_VALIDATION_INTERVAL : Long.parseLong(environment.getProperty("shiro.sessionManager.sessionValidationInterval")));
-        //全局会话超时时间（单位毫秒），缺省值为30分钟
-        defaultWebSessionManager.setGlobalSessionTimeout(environment.getProperty("shiro.sessionManager.globalSessionTimeout") == null
-                ? GLOBAL_SESSION_TIMEOUT : Long.parseLong(environment.getProperty("shiro.sessionManager.globalSessionTimeout")));
+        defaultWebSessionManager.setSessionValidationSchedulerEnabled(SESSION_VALIDATION_SCHEDULER_ENABLED);
         return defaultWebSessionManager;
     }
 
@@ -84,6 +68,13 @@ public class SecurityConfig {
         defaultWebSecurityManager.setRealm(oauth2Realm);
         defaultWebSecurityManager.setSessionManager(sessionManager);
         defaultWebSecurityManager.setRememberMeManager(null);
+        //本框架由于使用无状态登录，所以要禁用session
+        defaultWebSecurityManager.setSubjectFactory(new StatelessDefaultSubjectFactory(SESSION_CREATION_ENABLED));
+         /*
+            禁用使用Session作为存储策略的实现，但它没有完全地禁用Session
+            所以需要配合context.setSessionCreationEnabled(false);
+         */
+        ((DefaultSessionStorageEvaluator) ((DefaultSubjectDAO) defaultWebSecurityManager.getSubjectDAO()).getSessionStorageEvaluator()).setSessionStorageEnabled(SESSION_STORAGE_ENABLED);
         return defaultWebSecurityManager;
     }
 
