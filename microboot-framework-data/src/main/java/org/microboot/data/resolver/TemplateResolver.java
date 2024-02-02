1237d77fe8b6f391c6ddbe2470533a804670eeb2
hp525350557 <525350557@qq.com>
2024-02-02 16:14:22 +0800

    1、优化下代码

--- microboot-framework-data/src/main/java/org/microboot/data/resolver/TemplateResolver.java
+++ microboot-framework-data/src/main/java/org/microboot/data/resolver/TemplateResolver.java
@@ -41,34 +41,27 @@ public class TemplateResolver {
         }
         TemplateLoader[] templateLoaderArray = new TemplateLoader[resources.length];
         /*
-            之所以将变量放在循环外面，是为了提高性能
+            据说当循环比较大时，可以将一些变量放在循环外面来提高性能
             · 很多习惯写法是将变量直接放在循环内，那么每次循环都会产生一个新的引用指向一个新的对象
               循环不结束，那么GC不能回收之前的对象
             · 将变量写在循环外面，那么每次循环，引用指向一个新的对象，之前的对象就没有变量指向它了，GC时，即使循环不结束也能回收掉
+            对上述说法持保留意见，上课时学到的
          */
-        Resource resource;
-        InputStream in;
-        StringWriter writer;
-        String templateName;
-        String templateKey;
-        String templateContent;
-        StringTemplateLoader templateLoader;
         for (int i = 0; i < resources.length; i++) {
-            resource = resources[i];
-            templateName = resource.getFilename();
+            Resource resource = resources[i];
+            String templateName = resource.getFilename();
             if (!StringUtils.endsWith(templateName, ".sql")) {
                 continue;
             }
-            in = resource.getInputStream();
-            writer = new StringWriter();
-            IOUtils.copy(in, writer, StandardCharsets.UTF_8.name());
-            templateKey = StringUtils.replace(templateName, ".sql", "");
-            templateContent = writer.toString();
-            in.close();
-            writer.close();
-            templateLoader = new StringTemplateLoader();
-            templateLoader.putTemplate(templateKey, templateContent);
-            templateLoaderArray[i] = templateLoader;
+            try (InputStream in = resource.getInputStream();
+                 StringWriter writer = new StringWriter()) {
+                IOUtils.copy(in, writer, StandardCharsets.UTF_8.name());
+                String templateKey = StringUtils.replace(templateName, ".sql", "");
+                String templateContent = writer.toString();
+                StringTemplateLoader templateLoader = new StringTemplateLoader();
+                templateLoader.putTemplate(templateKey, templateContent);
+                templateLoaderArray[i] = templateLoader;
+            }
         }
         MultiTemplateLoader multiTemplateLoader = new MultiTemplateLoader(templateLoaderArray);
         this.configuration.setTemplateLoader(multiTemplateLoader);
@@ -78,10 +71,10 @@ public class TemplateResolver {
         if (template == null) {
             return null;
         }
-        StringWriter writer = new StringWriter();
-        template.process(parameters, writer);
-        String sql = writer.toString();
-        writer.close();
-        return sql;
+        try (StringWriter writer = new StringWriter()) {
+            template.process(parameters, writer);
+            String sql = writer.toString();
+            return sql;
+        }
     }
 }
